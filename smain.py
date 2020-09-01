f1d0f318cb39f0e7042fd1b487445ab4f9f2fc0a
PankajNk <68824616+PankajNk@users.noreply.github.com>
2020-09-02 00:07:35 +0530

    Add files via upload

---
+++ smain.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://www.fast2sms.com/dev/bulk"
+
+querystring = {"authorization":"P7vkROgY8CbFZKx9Turn3ol05AIMdXJ1qcySQfBs46wjzLEhW20hNTMtBYZQDlWPGHz1agUmv3K4yb28",
+               "sender_id":"FSTSMS",
+               "language":"english",
+               "route":"p",#promotn t for transcatn
+               "numbers":"74062042",
+               "message":"YOUR_QT_TEMPLATE_ID",
+               "variables":"{AA}|{CC}",
+               "variables_values":"12345|asdaswdx"}
+
+headers = {
+    'cache-control': "no-cache"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
