fecb3f9268aa70262d9f909455343ff4c9900159
PankajNk <68824616+PankajNk@users.noreply.github.com>
2020-09-02 00:09:56 +0530

    Update and rename smain.py to number_python_script.py

--- number_python_script.py
+++ number_python_script.py
@@ -2,11 +2,11 @@ import requests
 
 url = "https://www.fast2sms.com/dev/bulk"
 
-querystring = {"authorization":"P7vkROgY8CbFZKx9Turn3ol05AIMdXJ1qcySQfBs46wjzLEhW20hNTMtBYZQDlWPGHz1agUmv3K4yb28",
+querystring = {"authorization":"P7vkROgY8CbFZKx9Turn3ol05AIMdXJ1qcySQfBs46wjzLEhW20hNTMtBYZQDlWPGz1agUmv3K4yb28",
                "sender_id":"FSTSMS",
                "language":"english",
                "route":"p",#promotn t for transcatn
-               "numbers":"74062042",
+               "numbers":"74062042",#number you sign in 
                "message":"YOUR_QT_TEMPLATE_ID",
                "variables":"{AA}|{CC}",
                "variables_values":"12345|asdaswdx"}
