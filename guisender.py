6fac40729e24c4795a32f82788745d3a9f1094c5
PankajNk <68824616+PankajNk@users.noreply.github.com>
2020-09-01 23:55:10 +0530

    Add files via upload

---
+++ guisender.py
@@ -0,0 +1,22 @@
+import smtplib as s 
+ob=s.SMTP("smtp.gmail.com",587)
+
+ob.starttls()
+#tls is encryptn  
+#turn (less secure) in gmail
+ob.login("pankajrnaik317@gmail.com",'Password_of_email')
+
+subject="test for sending email using python"
+body="email is sent by using python  on 1st SePt...Happy Lockdown:):)"
+
+message = "Subject:{}\n\n{}".format(subject,body)
+#print(message)
+
+#In place of 1st email write the person gmail who you want to send 
+listOfAddress=["1st email.com","2nd gmail.com"]
+ob.sendmail("pankajrnaik317",listOfAddress,message)
+#take 3 parameter email from ,no of email and msg
+
+print("jale ra send")
+ob.quit()
+
